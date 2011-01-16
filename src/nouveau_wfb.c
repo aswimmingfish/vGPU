e8ce1ef351ee51bec51211dca6fda88fdbfbefee
Ben Skeggs <bskeggs@redhat.com>
2011-01-17 08:55:04 +1000

    fix hardcoding of nvc0 wfb path

--- src/nouveau_wfb.c
+++ src/nouveau_wfb.c
@@ -177,12 +177,12 @@ nouveau_wfb_setup_wrap(ReadMemoryProcPtr *pRead, WriteMemoryProcPtr *pWrite,
 		wfb->pitch = ppix->devKind;
 		/* 8192x8192x4 is 28 bits max, 64 - 28 == 36. */
 		wfb->multiply_factor = (((1ULL << 36) - 1) / wfb->pitch) + 1;
-		wfb->tile_height = bo->tile_mode + 2;
+		if (bo->device->chipset < 0xc0)
+			wfb->tile_height = bo->tile_mode + 2;
+		else
+			wfb->tile_height = (bo->tile_mode >> 4) + 3;
 		wfb->horiz_tiles = wfb->pitch / 64;
 		have_tiled = 1;
-
-		if (1 /* NV_ARCH_C0, FIXME */)
-			wfb->tile_height = (bo->tile_mode >> 4) + 3;
 	}
 
 out:
